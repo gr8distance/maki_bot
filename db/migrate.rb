cc66c55a4a73d7651038816f260aaed3af35d28f
UG <suzaku622@gmail.com>
2018-05-31 15:58:36 +0900

    テーブルの構成を変更した

--- db/migrate.rb
+++ db/migrate.rb
@@ -7,13 +7,13 @@ def migrate_all(force: false)
     t.string :id, primary_key: true
     t.string :text, null: false, default: ''
     t.string :tag_id, null: false
+    t.integer :weight, null: false
 
     t.timestamps
   end
 
   ActiveRecord::Migration.create_table(:tags, force: force, id: false) do |t|
     t.string :id, primary_key: true
-    t.integer :weight, null: false
 
     t.timestamps
   end
