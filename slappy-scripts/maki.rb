b3fdf49b6e4cc31d627e3576f6786985968ab9e6
UG <suzaku622@gmail.com>
2018-05-31 16:14:04 +0900

    更新されたまきちゃんのスクリプト

--- slappy-scripts/maki.rb
+++ slappy-scripts/maki.rb
@@ -1,3 +1,7 @@
+require './lib/models/serif'
+require './lib/models/tag'
+
+# FIXME: もしかしたらいらないかもしれないのでそのときは消す
 class String
   def to_kana
     tr('ぁ-ん', 'ァ-ン')
@@ -8,22 +12,25 @@ def maki(*word)
   if word.empty?
     '从廿_廿从'
   else
-    '从廿_廿从' + ' < ' + word.join('、').to_kana
+    '从廿_廿从' + ' < ' + word.join('、')
   end
 end
 
-class Team
-  def initialize
-    @name = ''
+def tags
+  @tags ||= Tag.all
+end
+
+hear %r{(まき|真姫|)ちゃ(ん|ーん)} do |e|
+  case e.text
+  when /.*すごい/
+    tag = tags.find_by(id: 'てれ')
+    say maki(Weight::Lottery.execute(tag.serifs).text), channel: e.channel
+  when /.*(かわ|可愛)いい/
+    tag = tags.find_by(id: 'かわいい')
+    say maki(Weight::Lottery.execute(tag.serifs).text), channel: e.channel
+  else
+    say maki('ゔぇえ', 'な', 'なによー'), channel: e.channel
   end
-end
-
-hear %r{(まき|真姫|)ちゃ(ん|ーん)$} do |event|
-  say maki('なによー'), channel: event.channel
-end
-
-hear %r{(まき|まきちゃん|西木野真姫).*(かわ|可愛)いい} do |event|
-  say maki('あたりまえでしょ！'), channel: event.channel
 end
 
 hear /うーん/ do |e|
@@ -33,23 +40,3 @@ end
 hear /筋肉/ do |e|
   say maki('イミワカンナい', 'きもちわるい'), channel: e.channel
 end
-
-# # use regexp in string literal
-# hear 'bar (.*)' do |event|
-#   puts event.matches[1] #=> Event#matches return MatchData object
-# end
-#
-#
-# # event object is slack event JSON (convert to Hashie::Mash)
-# hear '^bar (.*)' do |event|
-#   puts event.channel #=> channel id
-#   say 'slappy!', channel: event.channel #=> to received message channel
-#   say 'slappy!', channel: '#general'
-#   say 'slappy!', username: 'slappy!', icon_emoji: ':slappy:'
-# end
-#
-#
-# # use regexp literal
-# hear /^foobar/ do
-#   say 'slappppy!'
-# end
