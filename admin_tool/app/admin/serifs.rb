c6ad7c8fd3747ac287563ae7de53b3b3e1610d22
UG <suzaku622@gmail.com>
2018-05-31 18:26:00 +0900

    管理ツールからCSVを扱えるようにしたc

--- admin_tool/app/admin/serifs.rb
+++ admin_tool/app/admin/serifs.rb
@@ -1,15 +1,17 @@
 ActiveAdmin.register Serif do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  active_admin_importable do |model, hash|
+    model.create(id: hash[:id], text: hash[:text], tag_id: hash[:tag_id], weight: hash[:weight])
+  end
 end
