bb48eea8690206a07fb894dafbda1b9dcc0f42ab
UG <suzaku622@gmail.com>
2018-05-31 16:23:08 +0900

    maki.rbを更新した

--- slappy-scripts/maki.rb
+++ slappy-scripts/maki.rb
@@ -20,6 +20,10 @@ def tags
   @tags ||= Tag.all
 end
 
+def classic_songs
+  %w[愛の夢 月の光 月光 雨だれ Etude25-10]
+end
+
 hear %r{(まき|真姫|)ちゃ(ん|ーん)} do |e|
   case e.text
   when /.*すごい/
@@ -28,15 +32,17 @@ hear %r{(まき|真姫|)ちゃ(ん|ーん)} do |e|
   when /.*(かわ|可愛)いい/
     tag = tags.find_by(id: 'かわいい')
     say maki(Weight::Lottery.execute(tag.serifs).text), channel: e.channel
+  when /.*(筋肉|きんにく|muscle|Muscle)/
+    tag = tags.find_by(id: 'きんにく')
+    say maki(Weight::Lottery.execute(tag.serifs).text), channel: e.channel
+  when /.*クラシック/
+    classic_songs.each { |song| say maki(song), channel: e.channel }
+    say maki('とかがいいんじゃない？'), channel: e.channel
   else
     say maki('ゔぇえ', 'な', 'なによー'), channel: e.channel
   end
 end
 
 hear /うーん/ do |e|
-  say maki('いみわかんない！'), channel: e.channel
-end
-
-hear /筋肉/ do |e|
-  say maki('イミワカンナい', 'きもちわるい'), channel: e.channel
+  say maki('いみわかんない！'.to_kana), channel: e.channel
 end
