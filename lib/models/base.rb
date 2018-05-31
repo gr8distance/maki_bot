7cff9167637116f186dee36ae79053aa10996bdc
UG <suzaku622@gmail.com>
2018-05-31 16:06:35 +0900

    CSVインポート時にIDを決めるメソッドを追加した

--- lib/models/base.rb
+++ lib/models/base.rb
@@ -3,4 +3,8 @@ require 'active_record'
 class Base < ActiveRecord::Base
   establish_connection(ENV['DATABASE_URL'])
   self.abstract_class = true
+
+  def prepare_save!
+    nil
+  end
 end
--- lib/models/serif.rb
+++ lib/models/serif.rb
@@ -1,10 +1,14 @@
 require './lib/models/base'
 
 class Serif < Base
-  validates_presence_of :id, :text, :tag_id
+  validates_presence_of :id, :text, :tag_id, :weight
   belongs_to :tag
 
   before_validation do
+    self.id = text + '/' + tag_id
+  end
+
+  def prepare_save!
     self.id = text + '/' + tag_id
   end
 end
