09a25122e675d9fa9afb2b86a111bd5fe0e5e4a2
aimerald <aimerald3@gmail.com>
2018-06-04 22:21:11 +0900

    ちょっとコードの見通しが良くなった

--- slappy-scripts/maki.rb
+++ slappy-scripts/maki.rb
@@ -1,4 +1,5 @@
 require './lib/models/serif'
+require './lib/models/log'
 require './lib/models/active_channel'
 require './lib/string'
 
@@ -20,31 +21,35 @@ end
 
 hear %r{(まき|真姫)ちゃ(ん|ーん)} do |e|
   channel = reply(e)
-  case e.text
-  when /.*すごい/
-    say Serif.lottery_weight(tag).text, channel: channel
-  when /.*(かわ|可愛)いい/
-    say Serif.lottery_weight('かわいい').text, channel: channel
-  when /.*(筋肉|きんにく|muscle|Muscle)/
-    say Serif.lottery_weight('きんにく').text, channel: channel
-  else
-    # FIXME: ネガポジ判定Gemを入れてその内容に合わせて返事を抽選
-    say Serif.lottery_weight('その他').text, channel: channel
-  end
+  tag = case e.text
+        when /.*(すごい|天才|てんさい)/
+          'てれ'
+        when /.*(かわ|可愛)いい/
+          'かわいい'
+        when /.*(筋肉|きんにく|muscle|Muscle)/
+          'きんにく'
+        else
+          'その他'u
+        end
+  say Serif.lottery_weight(tag).text, channel: channel
+  Log.create!(channel: e.data.channel, message: e.text, tag: tag)
 end
 
 hear %r{.*(うーん|しんどい|疲れた|つかれた)} do |e|
   channel = reply(e)
   say Serif.lottery_weight('しんぱい').text, channel: channel
+  Log.create!(channel: e.data.channel, message: e.text, tag: 'しんぱい')
 end
 
 hear %r{(帰る|かえる)} do |e|
   channel = reply(e)
   say Serif.lottery_weight('おつかれ').text, channel: channel
+  Log.create!(channel: e.data.channel, message: e.text, tag: 'おつかれ')
 end
 
 hooks = '(眠|ねむ)い'
 hear %r{#{hooks}} do |e|
   channel = reply(e)
   say Serif.lottery_weight('通常').text, channel: channel
+  Log.create!(channel: e.data.channel, message: e.text, tag: '通常')
 end
