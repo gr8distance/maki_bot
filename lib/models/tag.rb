b863c61f7bc883e64212bc2a773b29dd9af647e1
aimerald <aimerald3@gmail.com>
2018-06-07 00:25:32 +0900

    タグがセリフ一覧を参照できるようにした

--- lib/models/tag.rb
+++ lib/models/tag.rb
@@ -1,7 +1,7 @@
 require './lib/models/base'
 
 class Tag < Base
-  has_many :serifs
+  has_many :serifs, primary_key: :name, foreign_key: :tag_id
 
   def lottery_weight
     ::Weight::Lottery.execute(serifs)
